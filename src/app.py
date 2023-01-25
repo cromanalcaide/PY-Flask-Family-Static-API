620178e40639d3ef190b191f11a0d886dfdf2443
cromanalcaide <cromanalcaide@gmail.com>
2023-01-25 20:24:00 +0000

    datastructure + app restitution

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
@@ -25,20 +23,32 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
+@app.route('/members/', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route('/member/', methods=['POST'])
+def add_member():
+    request_body = request.json
+    jackson_family.add_member(request_body)
+    return jsonify(request_body), 200
 
+@app.route('/member/<int:member_id>', methods=['GET'])
+def fetch_member(member_id):
+    member = jackson_family.get_member(member_id)
+    return jsonify(member), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    response_body = {"done": member}
+    if response_body == None:
+        return jsonify({"message": "Ha habido un error"}), 400
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,36 +1,47 @@
-
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
-        # example list of members
-        self._members = []
+        self._members = [{"id": self._generate_id(),
+                          "first_name": "John",
+                          "last_name": last_name,
+                          "age": 33,
+                          "lucky_numbers": [7, 13, 22]},
+                         {"id": self._generate_id(),
+                          "first_name": "Jane",
+                          "last_name": last_name,
+                          "age": 35,
+                          "lucky_numbers": [10, 14, 3]},
+                         {"id": self._generate_id(),
+                          "first_name": "Jimmy",
+                          "last_name": last_name,
+                          "age": 5,
+                          "lucky_numbers": [1]}]
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
+    def _generate_id(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member["id"] = self._generate_id()
+        member["last_name"] = self.last_name
+        self._members.append(member)
+        return self._members
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == self._members["id"]:
+                self._members.remove(member)
+                return True
+        else return False
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == self._members["id"]:
+                return member
 
-    # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+
